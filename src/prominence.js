104f5f9b3fbdb8bd8b6389b17dac7fc3881b07db
mohayonao <mohayonao@gmail.com>
2015-03-12 10:41:52 +0900

    add Prominence

--- src/prominence.js
+++ src/prominence.js
@@ -1,8 +1,40 @@
 "use strict";
 
-export default (context, method, args = []) => {
+class Prominence {
+  constructor(context) {
+    let methods = {};
+
+    for (let x = context; x != null; x = Object.getPrototypeOf(x)) {
+      Object.getOwnPropertyNames(x).forEach((name) => {
+        if (typeof context[name] === "function") {
+          methods[name] = true;
+        }
+      });
+    }
+
+    let proxy = (name, args) => {
+      return new Promise((resolve, reject) => {
+        context[name].apply(context, args.concat((err, result) => {
+          if (err) {
+            return reject(err);
+          }
+          return resolve(result);
+        }));
+      });
+    };
+
+    Object.keys(methods).forEach((name) => {
+      this[name] = (...args) => proxy(name, args);
+    });
+  }
+}
+
+export default (context, methodName = null, args = []) => {
+  if (methodName === null) {
+    return new Prominence(context);
+  }
   return new Promise((resolve, reject) => {
-    context[method].apply(context, args.concat((err, result) => {
+    context[methodName].apply(context, args.concat((err, result) => {
       if (err) {
         return reject(err);
       }
